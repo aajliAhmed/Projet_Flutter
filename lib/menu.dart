da3d5948a08c503716df293effd5cfaf35fea309
AAJLI Ahmed <ahmed.conceptor@gmail.com>
2023-12-05 15:47:10 +0100

    ajoute une widget avec image

--- lib/menu.dart
+++ lib/menu.dart
@@ -1,12 +1,18 @@
-
 import 'package:flutter/material.dart';
-
 
 class Main extends StatelessWidget {
   const Main({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Drawer(
+      backgroundColor: Colors.green[100],
+      child: ListView(
+        children: [
+          DrawerHeader(child: CircleAvatar(backgroundImage: AssetImage("../assets/ai.jpeg"),))
+        ]
+        
+        ),
+    );
   }
 }
