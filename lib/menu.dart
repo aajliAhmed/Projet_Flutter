30ceca9bb4c9b622a97dd734b4beda084bd0b1d6
AAJLI Ahmed <ahmed.conceptor@gmail.com>
2023-12-06 15:59:24 +0100

    ajoute un background verte avec un widget home

--- lib/menu.dart
+++ lib/menu.dart
@@ -7,9 +7,15 @@ class Main extends StatelessWidget {
   Widget build(BuildContext context) {
     return Drawer(
       backgroundColor: Colors.green[100],
+
       child: ListView(
         children: [
-          DrawerHeader(child: CircleAvatar(backgroundImage: AssetImage("../assets/ai.jpeg"),))
+          
+          DrawerHeader(
+            decoration: BoxDecoration(gradient: LinearGradient(colors: [Colors.blue,Colors.green,Colors.white])),
+            child: CircleAvatar(backgroundImage: AssetImage("../assets/ai.jpeg"),)),
+            ExpansionTile(title: Text("Home"),leading: Icon(Icons.home),)
+            
         ]
         
         ),
